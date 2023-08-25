d2087bc9c35702caf3d47ce0694ed17451762980
Anders Kvalvik Kvernberg <anders.kvernberg@recognition.no>
2023-08-25 22:24:37 +0200

    Add zero padding to the timer
    Examples:
    * "0:0"  => "00:00"
    * "0:5"  => "00:05"
    * "0:11" => "00:11"
    * "10:2" => "10:02

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
+function zeroPad(n: number) {
+	return n >= 10 ? n.toString() : `0${n}`;
+}
+
 export function formatTime(milliseconds: number) {
-	const ss = Math.floor(milliseconds / 1000) % 60;
-	const mm = Math.floor(milliseconds / 1000 / 60);
+	const ss = zeroPad(Math.floor(milliseconds / 1000) % 60);
+	const mm = zeroPad(Math.floor(milliseconds / 1000 / 60));
 
 	return `${mm}:${ss}`;
 }
