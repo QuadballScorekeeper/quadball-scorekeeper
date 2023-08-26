a62ce9e79713656a1cec9c96dc4d44a485f75aa5
Stein Elgethun <26071973+stonemoose@users.noreply.github.com>
2023-08-26 15:06:10 +0200

    Add basic score counter (#25)
    resolves #4

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,33 +1,24 @@
 <script lang="ts">
-	import { spring } from 'svelte/motion';
+	// import { spring } from 'svelte/motion';
+	import { writable } from 'svelte/store';
 
-	let count = 0;
-
-	const displayed_count = spring();
-	$: displayed_count.set(count);
-	$: offset = modulo($displayed_count, 1);
-
-	function modulo(n: number, m: number) {
-		// handle negative numbers
-		return ((n % m) + m) % m;
-	}
+	let counter = writable(0);
 </script>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+	<button on:click={() => counter.update((n) => n && n - 10)} aria-label="Decrease the counter">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
 	</button>
 
 	<div class="counter-viewport">
-		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
-			<strong class="hidden" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
-			<strong>{Math.floor($displayed_count)}</strong>
+		<div class="counter-digits">
+			<strong>{$counter}</strong>
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<button on:click={() => counter.update((n) => n + 10)} aria-label="Increase the counter">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
@@ -93,10 +84,5 @@
 		position: absolute;
 		width: 100%;
 		height: 100%;
-	}
-
-	.hidden {
-		top: -100%;
-		user-select: none;
 	}
 </style>
