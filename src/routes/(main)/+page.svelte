c8e36e37bc69124c12bcd586fc8a0d80060ff36f
Stein Elgethun <stein.elgethun@gmail.com>
2026-03-20 13:59:51 +0100

    Hide admin panel button

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -8,7 +8,7 @@
 <main>
 	<Button href="/tournaments" size="large">View tournaments</Button>
 	<Button href="/scorekeeper" size="large">Start a game</Button>
-	<Button href="/admin" size="large">Admin panel</Button>
+	<!-- <Button href="/admin" size="large">Admin panel</Button> -->
 </main>
 
 <style>
